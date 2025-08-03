63ca81c951c8385357fe86d2885591a1486a24ad
emran666666 <149528745+emran666666@users.noreply.github.com>
2025-08-03 10:55:45 +0300

    Add Toasts container component to display list of notifications

---
+++ src/components/layout/ToastContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="fixed top-4 left-4 z-50 space-y-2 w-80">
+    <transition-group name="fade">
+      <Toast v-for="toast in toasts" :key="toast.id" :toast="toast" />
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Toast from './Toast.vue'
+
+export default {
+  components: { Toast },
+  computed: {
+    ...mapState('toasts', ['toasts'])
+  }
+}
+</script>
