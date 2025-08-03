9b4cd505e7f8a26f97b69dfcdad347e1316dcb4e
emran666666 <149528745+emran666666@users.noreply.github.com>
2025-08-03 10:05:50 +0300

    Add volunteers page with form, list, and admin modal

---
+++ src/views/VolunteersView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="max-w-7xl mx-auto">
+    <AppHeader :title="'Volunteers'" :showAddButton="user.role === 'admin'" @add="showModal = true" />
+    
+    <div class="bg-white dark:bg-gray-800 rounded-xl shadow-md p-6 transition-all">
+      <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <VolunteerForm />
+        <VolunteerList />
+      </div>
+    </div>
+    
+    <Modal v-if="showModal" :modalType="'volunteer'" @close="showModal = false" />
+  </div>
+</template>
+
+<script>
+import AppHeader from '@/components/layout/AppHeader.vue'
+import VolunteerForm from '@/components/volunteers/VolunteerForm.vue'
+import VolunteerList from '@/components/volunteers/VolunteerList.vue'
+import Modal from '@/components/layout/Modal.vue'
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    AppHeader,
+    VolunteerForm,
+    VolunteerList,
+    Modal
+  },
+  data() {
+    return {
+      showModal: false
+    }
+  },
+  computed: {
+    ...mapState('user', ['user'])
+  }
+}
+</script>
